bb52c81f95901c8ba214a19e32b6130ae0c6632c
SubjectZeroo <jasordev@gmail.com>
2022-08-05 12:33:02 -0400

    #5 Rutas para Article

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('articles', function () {
+    return Article::all();
+});
+
+Route::get('articles/{article}', function ($article) {
+    return Article::find($article);
+});
+
+Route::post('articles', function (Request $request) {
+    return Article::create($request->all());
+});
+
+
+Route::put('articles/{id}', function (Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+
+Route::delete('articles/{article}', function ($article) {
+    Article::find($article)->delete();
+    return 204;
 });
